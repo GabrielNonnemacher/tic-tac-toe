6daecfe626cb949321dfa88ae09476841ceaba76
Gabriel Nonnemacher <nonnemacher99gabriel@gmail.com>
2024-01-03 23:31:14 -0300

    Task #6 - adicionando métodos bases

--- src/app/tic-tac-toe/shared/tic-tac-toe.service.ts
+++ src/app/tic-tac-toe/shared/tic-tac-toe.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TicTacToeService {
-  
+
   private readonly SIZE_BOARD: number = 3;
   private readonly X: number = 1;
   private readonly O: number = 2;
@@ -18,6 +18,43 @@ export class TicTacToeService {
   private _showStart?: boolean;
   private _showBoard?: boolean;
   private _showEnd?: boolean
-  
-  constructor() { }
+
+  constructor() {}
+
+  starter(): void {
+    this._showStart = true;
+    this._showBoard = false;
+    this._showEnd = false;
+    this.numActions = 0;
+    this.victory = false;
+    this.startBoard();
+  }
+
+  startBoard(): void {
+    this.board = [this.SIZE_BOARD];
+    for (let i = 0; i < this.SIZE_BOARD; i++) {
+      this.board[i] = [this.EMPTY, this.EMPTY, this.EMPTY]
+    }
+  }
+
+  get showStart(): boolean | undefined {
+    return this._showStart;
+  }
+
+  get showBoard(): boolean | undefined {
+    return this._showBoard;
+  }
+
+  get showEnd(): boolean | undefined {
+    return this._showEnd;
+  }
+
+  get player(): number | undefined {
+    return this._player;
+  }
+
+  startGame(): void {
+    this._showStart = false;
+    this._showBoard = true;
+  }
 }
